7254bf87a0d36165d17a7598f0d458503d39df33
test <zzhmark@github.com>
2021-05-28 15:09:18 +0800

    composeFiel

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com.cnpmjs.org/zzhmark/ccf-db.git
-git clone https://github.com/zzhmark/ccf-db-backend.git
+git clone https://hub.fastgit.org/zzhmark/ccf-db.git
+git clone https://hub.fastgit.org/zzhmark/ccf-db-backend.git
 
-sudo docker-compose up
+docker-compose up
